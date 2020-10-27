bc34070a78a0415af70c0ddedbba50c661db369c
Naveen Bandarage <thebanterage@gmail.com>
2020-10-27 23:43:27 +1300

    Commiting basic version

--- SalesTax/ViewController.swift
+++ SalesTax/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //these are the fields.
+    @IBOutlet weak var totalPriceLabel: UILabel!
+    @IBOutlet weak var salesTaxTxt: UITextField!
+    @IBOutlet weak var priceTxt: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        totalPriceLabel.text = ""
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTotalPrice(_ sender: Any) {
+        let price = Double(priceTxt.text!)! //convert this into decimal this ! makes sure that there is text there.
+        let salesTax = Double(salesTaxTxt.text!)!
+        let totalSalesTax = price * salesTax
+        let totalPrice = price + totalSalesTax
+        totalPriceLabel.text = "$\(totalPrice)" // converts to a string"
+    }
+    
 }
 
